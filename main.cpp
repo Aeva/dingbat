7e9da47e7274bdb56e8394e2acd656852ba90666
Aeva Palecek <aeva@lunarfracking.com>
2017-11-05 12:22:23 -0600

    blank window

--- main.cpp
+++ main.cpp
@@ -2,15 +2,38 @@
 #define GLFW_INCLUDE_ES31
 #include <GLFW/glfw3.h>
 
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+
 int main()
 {
-    if (glfwInit())
-    {
-    }
-    else
+    if (!glfwInit())
     {
         return 1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+	return 1;
+    }
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     glfwTerminate();
     return 0;
 }
